3b47c8a4556601c988ea25906a7db889a4abf184
bolunwu <bolunwu@tencent.com>
2025-05-18 01:09:43 +0800

    refactor: add util dir

--- llm_siri/cli/run_server.py
+++ llm_siri/cli/run_server.py
@@ -4,6 +4,6 @@
 import os
 
 from llm_siri.core.service import LLMService
-from llm_siri.utils import norm_path
+from llm_siri.utils.misc import norm_path
 
 llm_service_app = LLMService.bind(model_path=norm_path(os.getenv("MODEL_PATH")))
